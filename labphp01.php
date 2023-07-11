ab03d3bb59819f759a6748bbe3744a97c7ed2486
PKS Amasha <lukasu713@gmail.com>
2023-07-11 11:42:52 +0530

    Add files via upload

---
+++ labphp01.php
@@ -0,0 +1,20 @@
+<html>
+<body>
+
+<?php
+$x =6;
+$y=4; 
+
+$sum =$x + $y;
+$difference = $x -$y;
+$product =$x*$y;
+$division= $x/$y;
+
+echo "Sum: $sum" ;
+echo "<br>Difference: $difference" ;
+echo "<br>Product: $product";
+echo "<br>Division: $division" ;
+
+?>
+</body>
+</html>
